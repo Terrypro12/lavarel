b4ff445e6da05f462a36e5aa7597a18d7d96a31e
bryanzt139008 <bryanbozt139008@gmail.com>
2023-11-23 10:09:08 -0500

    2v viernes 23/11

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+Route::get('/', [PagesController::class, 'fnIndex']) -> name('xInicio');
+
+Route::get('/galeria/{numero?}', [PagesController::class, 'fnGaleria']) -> where('numero', '[0-9]+') -> name('xGaleria');
+
+Route::get('/lista', [PagesController::class, 'fnlista']) -> name('xLista');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+}) -> name('xInicio');
+
+
+
+Route::get('/saludo', function () {
+    return "Hola mundo desde Laravel xd....";
+});
+
+Route::get('/galeria/{numero?}', function ($numero=null) {
+    return "Este es el codigo de la foto: ".$numero;
+}) -> where('numero', '[0-9]+');
+
+Route ::view('/galeria', 'pagGaleria', ['valor' => 15]) -> name('xGaleria');
+
+Route::get('/lista', function (){
+    return view('pagLista');
+}) -> name('xLista');
+*/
+
+Route::get('/dashboard', function (){
+    return view('dashboard');
+}) -> middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require _DIR_.'/auth.php';
